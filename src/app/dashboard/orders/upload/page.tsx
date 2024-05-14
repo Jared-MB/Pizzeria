165d7d146472255e989bd0ed91af3b320a844e4c
Jared Muñoz <amunozbaez669@gmail.com>
2024-05-13 21:57:54 -0600

    suspense

--- src/app/dashboard/orders/upload/page.tsx
+++ src/app/dashboard/orders/upload/page.tsx
@@ -4,14 +4,16 @@ import UploadOrderForm from "./components/form";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Suspense } from "react";
 
 const queryClient = new QueryClient();
 
 export default function UploadOrderPage() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<UploadOrderForm />
-			<ReactQueryDevtools />
+			<Suspense>
+				<UploadOrderForm />
+			</Suspense>
 		</QueryClientProvider>
 	);
 }
